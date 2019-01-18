95bae9dec12663ab134dc1bcc440661c737e36be
sowmya <sowmyatsrinivas@gmail.com>
2019-01-18 17:31:38 +0530

    Add files via upload

---
+++ rest_tutorial/src/main/java/io/target/springproject/rest_tutorial/RestTutorialApplication.java
@@ -0,0 +1,14 @@
+package io.target.springproject.rest_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutorialApplication.class, args);
+	}
+
+}
+
