7ec56f5b500144b4534f53fd14b0563e634197f0
sowmya <sowmyatsrinivas@gmail.com>
2019-01-24 11:14:25 +0530

    Add files via upload

---
+++ springAuthentication/src/main/java/io/target/springproject/Authentication/AuthenticationApplication.java
@@ -0,0 +1,14 @@
+package io.target.springproject.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
+
